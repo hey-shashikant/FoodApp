90be7ec7908102e9f06f9060a6f97eb871b2841c
hey-shashikant <shashikantsolanki041@gmail.com>
2024-06-20 19:52:22 +0530

    Added on-demand loading

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,7 @@ import ResturantCard from "./ResturantCard";
 import { useState, useEffect } from "react";
 import Shimmer from "./Shimmer";
 import { Link } from "react-router-dom";
+import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Body = () => {
     // Local State Variable - Super powerful variable
@@ -27,6 +28,12 @@ const Body = () => {
         setFilteredRestaurant(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
 
     }; 
+
+    const onlineStatus = useOnlineStatus();
+
+    if (onlineStatus === false) {
+        return <h1>Looks like you're offline!! Please check your internet connection</h1>
+    };
 
     return listOfResturants.length === 0 ? <Shimmer /> : (
         <div className="body">
