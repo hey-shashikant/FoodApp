8c650ded3421c8b3fcafbc7f5f01c2b7c02e7d09
hey-shashikant <shashikantsolanki041@gmail.com>
2024-06-21 10:38:49 +0530

    fixed css

--- src/components/ResturantCard.js
+++ src/components/ResturantCard.js
@@ -1,29 +1,29 @@
 import { CDN_URL } from "../utils/constants";
 
 const ResturantCard = (props) => {
-    const { resData } = props;
+  const { resData } = props;
 
-    const {
-        cloudinaryImageId,
-        name, 
-        avgRatingString,
-        cuisines,
-        costForTwo,
-        sla,
-    } = resData?.info;
+  const {
+    cloudinaryImageId,
+    name,
+    avgRatingString,
+    cuisines,
+    costForTwo,
+    sla,
+  } = resData?.info;
 
-    const { deliveryTime } = sla;
+  const { deliveryTime } = sla;
 
-    return (
-        <div className="m-4 p-4 w-[250px] rounded-lg bg-gray-100 hover:bg-gray-200">
-            <img className="res-logo rounded-lg" alt="res-logo" src={CDN_URL + cloudinaryImageId}/>
-            <h3 className="font-bold py-4 text-lg">{name}</h3>
-            <h4>{cuisines.join(", ")}</h4>
-            <h4>{avgRatingString} stars</h4>
-            <h4>{deliveryTime} minutes</h4>
-            <h4>{costForTwo}</h4>
-        </div>
-    )
-}
+  return (
+    <div className="restaurant-card m-4 p-4 w-[250px] h-[350px] rounded-lg bg-gray-100 hover:bg-gray-200">
+      <img className="res-logo rounded-lg w-full h-32 object-cover" alt="res-logo" src={CDN_URL + cloudinaryImageId} />
+      <h3 className="font-bold py-2 text-lg">{name}</h3>
+      <h4 className="text-sm truncate">{cuisines.join(", ")}</h4>
+      <h4 className="text-sm">{avgRatingString} stars</h4>
+      <h4 className="text-sm">{deliveryTime} minutes</h4>
+      <h4 className="text-sm">{costForTwo}</h4>
+    </div>
+  );
+};
 
-export default ResturantCard
+export default ResturantCard;
