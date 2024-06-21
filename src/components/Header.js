78e0450e157274691b31266cf7e69fae526ca7ee
hey-shashikant <shashikantsolanki041@gmail.com>
2024-06-21 10:41:30 +0530

    added button to display online/offline status

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,40 +4,50 @@ import { Link } from "react-router-dom";
 import useOnlineStatus from "../utils/useOnlineStatus";
 
 const Header = () => {
+  const [btnName, setBtnName] = useState("Login");
 
-    // let btnName = "Login"
-    const [btnName, setBtnName] = useState("Login");
+  const onlineStatus = useOnlineStatus();
 
-    const onlineStatus = useOnlineStatus();
-
-    // if no dependency array => useEffect is called on every component render
-    // there is a catch if dependency array is present than useEffect will be called only 
-    // at first render but if dependency array is not present it will be called after every render
-    // if the dependency array is like [btnName] then every time when btn is updated it will be called
-    // useEffect ( () =>  {
-    //     console.log("useEffect called")
-    // }, [])
-
-    return (
-        <div className="flex justify-between bg-pink-100 shadow-lg">
-            <div className="logo-container">
-                <img className="w-56" src={LOGO_URL}/>
-            </div>
-            <div className="flex items-center">
-                <ul className="flex p-4 m-4">
-                    <li className="px-4">Online Status: {onlineStatus ? "Online" : "Offline"}</li>
-                    <li className="px-4"> <Link to="/"> Home </Link> </li>
-                    <li className="px-4"> <Link to="/about"> About Us</Link> </li>
-                    <li className="px-4"> <Link to="/contact"> Contact Us</Link> </li>
-                    <li className="px-4"> <Link to="/grocery"> Grocery</Link> </li>
-                    <li className="px-4">Cart</li>
-                    <button className="login" onClick={() => {
-                        btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
-                    }}>{btnName}</button>
-                </ul>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex justify-between bg-pink-100 shadow-lg">
+      <div className="logo-container">
+        <img className="w-56" src={LOGO_URL} alt="Company Logo" />
+      </div>
+      <div className="flex items-center">
+        <ul className="flex p-4 m-4 items-center">
+          <li className="px-4 flex items-center">
+            <span
+              className={`ml-2 w-3 h-3 rounded-full ${
+                onlineStatus ? "bg-green-500" : "bg-red-500"
+              }`}
+            ></span>
+            {onlineStatus ? "" : ""}
+          </li>
+          <li className="px-4">
+            <Link to="/"> Home </Link>
+          </li>
+          <li className="px-4">
+            <Link to="/about"> About Us</Link>
+          </li>
+          <li className="px-4">
+            <Link to="/contact"> Contact Us</Link>
+          </li>
+          <li className="px-4">
+            <Link to="/grocery"> Grocery</Link>
+          </li>
+          <li className="px-4">Cart</li>
+          <button
+            className="px-4 py-2 bg-gray-100 rounded-lg"
+            onClick={() => {
+              btnName === "Login" ? setBtnName("Logout") : setBtnName("Login");
+            }}
+          >
+            {btnName}
+          </button>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
